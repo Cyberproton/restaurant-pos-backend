8183970d6bbddb9c9ad75de94489f799d9a11022
Cyberproton <cyberprotonAtMailbox@gmail.com>
2021-05-13 16:43:49 +0700

    init

--- routes/hello.js
+++ routes/hello.js
@@ -2,20 +2,8 @@ const express = require('express')
 const { postHello, getHello } = require('../controllers/hello')
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
-    res.send(JSON.stringify({ helloMessage: "Hello World!" }))
-})
-
-router.post('/', (req, res, next) => {
-    let hello = "Hello World"
-    if (req.body.helloMessage) {
-        hello = req.body.helloMessage
-    }
-    res.send(JSON.stringify({ helloMessage: hello }))
-})
-
-//router.route('/')
-//    .post(postHello)
-//    .get(getHello)
+router.route('/')
+    .post(postHello)
+    .get(getHello)
 
 module.exports = router
